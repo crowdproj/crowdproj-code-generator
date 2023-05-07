fb3a38c8e196c2868e1102d1361e554567699f7b
Sergey Okatov <sokatov@gmail.com>
2023-05-07 18:07:22 +0500

    fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 group = "com.crowdproj.generator"
-version = "0.0.0"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
--- crowdproj-generator-plugin/build.gradle.kts
+++ crowdproj-generator-plugin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.crowdproj.generator"
-version = "0.0.6"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
