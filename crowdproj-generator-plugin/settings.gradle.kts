14033833dfab9497f0b914826b71f474f2a5b016
Sergey Okatov <svok@users.noreply.github.com>
2023-08-29 17:55:42 +0500

    230829 version (#3)
    * Version of Kotlin and Gradle have been updated to 1.9.0 and 8.3 respectively
    
    * Version of Kotlin and Gradle have been updated to 1.9.0 and 8.3 respectively
    
    * Version of Kotlin and Gradle have been updated to 1.9.0 and 8.3 respectively

--- crowdproj-generator-plugin/settings.gradle.kts
+++ crowdproj-generator-plugin/settings.gradle.kts
@@ -1,13 +1,11 @@
 pluginManagement {
     plugins {
         val kotlinVersion: String by settings
-        val shadowVersion: String by settings
         val dokkaVersion: String by settings
         val nexusStagingVersion: String by settings
         val pluginPublishVersion: String by settings
 
         kotlin("jvm") version kotlinVersion
-        id("com.github.johnrengelman.shadow") version shadowVersion
         id("org.jetbrains.dokka") version dokkaVersion
         id("io.codearte.nexus-staging") version nexusStagingVersion
         id("com.gradle.plugin-publish") version pluginPublishVersion
