57492cacf83cfb46790c923bf9bb231b97c0de7c
Sergey Okatov <svok@users.noreply.github.com>
2025-02-07 10:22:22 +0500

    versions updates (#4)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,3 @@
-pluginManagement {
-    plugins {
-        val serializationVersion: String by settings
-        val kotlinVersion: String by settings
-        val dokkaVersion: String by settings
-        val nexusStagingVersion: String by settings
-
-        kotlin("jvm") version kotlinVersion
-        kotlin("multiplatform") version kotlinVersion
-        id("org.jetbrains.dokka") version dokkaVersion
-        id("io.codearte.nexus-staging") version nexusStagingVersion
-        kotlin("plugin.serialization") version serializationVersion
-    }
-}
-
 rootProject.name = "crowdproj-generator"
 
 includeBuild("crowdproj-generator-plugin")
